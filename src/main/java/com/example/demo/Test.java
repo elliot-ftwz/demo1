30a62886c5b0019bbb4d33f5997774d9f9012b0d
elliot <elliot@Fui-elliot-XGHJXLHQC7.local>
2023-07-12 17:53:19 +0900

    Initial commit

---
+++ src/main/java/com/example/demo/Test.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+public class Test {
+
+    public static void main(String[] args){
+        boolean t = true;
+
+        if(t == true && t){
+            System.out.println("123123123");
+        }
+    }
+}
