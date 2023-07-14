da2580df89353f318a1a808a1bfb1dd45f8b29b2
elliot <elliot@futurewiz.co.kr>
2023-07-14 16:11:17 +0900

    test

---
+++ src/main/java/com/example/demo/Test3.java
@@ -0,0 +1,11 @@
+package com.example.demo;
+
+public class Test3 {
+
+    public void prTest(){
+        String id = "test";
+        String password = "1111";
+
+        System.out.println(id + " " + password);
+    }
+}
